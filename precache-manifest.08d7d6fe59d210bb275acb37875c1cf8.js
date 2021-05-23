0b390d182d76f0decf4c15bca3e65a5392782895
VassilinaPap <vassilinapapadouli@gmail.com>
2021-05-23 15:41:20 +0300

    Updates

---
+++ precache-manifest.08d7d6fe59d210bb275acb37875c1cf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7035d0c82f9db836a49a0b6b93656d72",
+    "url": "/AuctionApp/index.html"
+  },
+  {
+    "revision": "3fc0497397a919533897",
+    "url": "/AuctionApp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1c9f00b7aa3873fc3e53",
+    "url": "/AuctionApp/static/js/2.a78fbedf.chunk.js"
+  },
+  {
+    "revision": "3fc0497397a919533897",
+    "url": "/AuctionApp/static/js/main.8906f067.chunk.js"
+  },
+  {
+    "revision": "d314b8a2230841a2699b",
+    "url": "/AuctionApp/static/js/runtime-main.3e6a917d.js"
+  }
+]);
